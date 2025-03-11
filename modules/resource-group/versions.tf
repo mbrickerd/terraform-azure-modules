346b815a43206429212c1239d542cc48eb8d6334
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-03-11 21:28:30 +0100

    fix: allow providers block with aliases

--- modules/resource-group/versions.tf
+++ modules/resource-group/versions.tf
@@ -3,8 +3,9 @@ terraform {
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      source                = "hashicorp/azurerm"
+      version               = "~> 4.0"
+      configuration_aliases = [azurerm]
     }
   }
 }
