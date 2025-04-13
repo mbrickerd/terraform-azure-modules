2b69096bd4a886273145d2ed7ec8b62c4767fe19
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-04-13 13:20:39 +0200

    fix: remove container_app_environment_id

--- modules/container-app/locals.tf
+++ modules/container-app/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  law_name = "law-${var.name}-${var.environment}"
-  cae_name = "cae-${var.name}-${var.environment}"
-  app_name = "app-${var.name}-${var.environment}"
-  tags     = merge(var.tags, { environment = var.environment })
+  naming_convention = "${var.name}-${var.environment}"
+  law_name          = "law-${local.naming_convention}"
+  cae_name          = "cae-${local.naming_convention}"
+  app_name          = "app-${local.naming_convention}"
+  tags              = merge(var.tags, { environment = var.environment })
 }
