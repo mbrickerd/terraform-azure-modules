d6373bdf88a77f0b8d19672c28caf14fec166a3c
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-05-03 10:48:50 +0200

    fix: simplified App Registration module

--- modules/app-registration/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  scope = "/subscriptions/${var.subscription_id}"
-}
--- modules/app-registration/variables.tf
+++ modules/app-registration/variables.tf
@@ -1,14 +1,4 @@
-variable "subscription_id" {
-  description = "The subscription ID to grant the app registration service principal access to."
-  type        = string
-}
-
 variable "display_name" {
   description = "The App Registration name displayed in the Azure Portal."
-  type        = string
-}
-
-variable "role_definition_name" {
-  description = "The role to assign the App Registration over the subscription."
   type        = string
 }
